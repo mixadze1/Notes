b71bf71b99b1075792327b40ccc4913cf53dfe67
Mihail A <mixadze1@bk.ru>
2023-02-19 17:19:38 +0300

    add id delete note command

--- Notes.Application/Notes/Commands/DeleteCommand/DeleteNoteCommand.cs
+++ Notes.Application/Notes/Commands/DeleteCommand/DeleteNoteCommand.cs
@@ -10,5 +10,7 @@ namespace Notes.Application.Notes.Commands.DeleteCommand
     public class DeleteNoteCommand : IRequest
     {
         public Guid UserId {get; set; }
+        public Guid Id { get; set; }
+
     }
 }
